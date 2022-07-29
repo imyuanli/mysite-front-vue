a2f7f05e61303a2f22f8051ec5a596236d50b834
yuanli <2865437316@qq.com>
2022-07-29 17:57:23 +0800

    随机img

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,10 +1,11 @@
 <template>
-  <view class="content">
+  <view class="content" :style="{'backgroundImage':`url(${backgroundImage})`}">
     <view :class="[showLongInput?'contentBox showBlur':'contentBox']">
-      <view class="time">{{dateTime}}
+      <view class="time">
+        <span>{{dateTime}}</span>
+        <span v-if="showSeconds">{{dateSeconds}}</span>
       </view>
       <view :class="[showLongInput?'inputBox longInput ':'inputBox shortInput']">
-<!--        <view :class="['inputBox longInput ']">-->
         <view class="searchBar">
           <input @focus="handleClick"
                  @blur="handleClick"
@@ -16,24 +17,29 @@
       <view v-if="showLongInput ">
         <Tools />
       </view>
+      <view class="reflash">
+        <view @click="getBackgroundImage">
+          <svg t="1659080303977" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="7004" width="30" height="30"><path d="M960 416V192l-73.056 73.056a447.712 447.712 0 0 0-373.6-201.088C265.92 63.968 65.312 264.544 65.312 512S265.92 960.032 513.344 960.032a448.064 448.064 0 0 0 415.232-279.488 38.368 38.368 0 1 0-71.136-28.896 371.36 371.36 0 0 1-344.096 231.584C308.32 883.232 142.112 717.024 142.112 512S308.32 140.768 513.344 140.768c132.448 0 251.936 70.08 318.016 179.84L736 416h224z" p-id="7005" fill="#8a8a8a"></path></svg>
+        </view>
+      </view>
     </view>
   </view>
 </template>
 
 <script>
-import {get_source_list} from "../../service/service";
+import {get_login_code,get_background_mage} from "../../service/service";
 import Tools from '../../compontent/tools'
-
-export default {
+import axios from "axios";
+export default  {
   components: {
-    Tools,
+    Tools
   },
   data() {
     return {
       inputVlaue: '',
       showLongInput: false,
-      dateTime:'',
-      options: [{
+      options: [
+          {
         value: '选项1',
         label: '黄金糕'
       }, {
@@ -50,14 +56,22 @@ export default {
         value: '选项5',
         label: '北京烤鸭'
       }],
-      value: ''
+      value: '',
+      //  时间
+      dateTime:'',
+      dateSeconds:"",
+      showSeconds:false,
+      timer:"",
+    //壁纸
+      backgroundImage:''
     }
   },
   onLoad() {
+    this.getBackgroundImage()
+    this.timer = setInterval(()=>{
+      this.getNowTime()
+    }, 1000);
 
-    get_source_list().then((res) => {
-      console.log(res)
-    });
   },
   methods: {
     handleClick() {
@@ -65,15 +79,27 @@ export default {
     },
     getNowTime(){
       let date = new Date()
-      this.dateTime = `${date.getHours()}:${date .getMinutes()}:${date.getSeconds()}`
+      this.dateTime = `${date.getHours()}:${date.getMinutes()}`
+      this.dateSeconds = `:${date.getSeconds()}`
+    },
+    getBackgroundImage(){
+      get_background_mage().then(
+          (res)=>{
+            this.backgroundImage = res
+          }
+      )
     }
-  }
+  },
+  beforeDestroy() {
+    if (this.timer) {
+      clearInterval(this.timer); // 在Vue实例销毁前，清除我们的定时器
+    }
+  },
 }
 </script>
 
 <style>
 .content {
-  background-image: url("../../static/backimg.jpg");
   background-size: cover;
   background-position: center;
   background-repeat: no-repeat;
@@ -82,7 +108,19 @@ export default {
   position: relative;
 }
 .time{
-
+  position: fixed;
+  top: 120px;
+  left: 50%;
+  transform: translateX(-50%);
+  padding: 10px;
+  text-align: center;
+  cursor: pointer;
+  transition: .25s;
+  animation: delayedFadeIn 2s;
+  color: #fff;
+  font-size: 32px;
+  font-weight: 200;
+  text-shadow: 0 0 20px rgba(0,0,0,.35);
 }
 .contentBox {
   width: 100%;
@@ -96,7 +134,7 @@ export default {
 
 .inputBox {
   position: absolute;
-  top: 220px;
+  top: 200px;
   left: 50%;
   transform: translateX(-50%);
   width: 230px;
@@ -153,4 +191,10 @@ export default {
   z-index: 1000;
 }
 
+.reflash{
+  position: absolute;
+  right: 50px;
+  top: 20px;
+  cursor: pointer;
+}
 </style>
--- service/service.js
+++ service/service.js
@@ -1,52 +1,9 @@
 import request from '../service/request'
-const BASE_URL = 'http://127.0.0.1:8000'
-
-export async function get_login(payload) {
-  return request.post(BASE_URL + '/api/get_login/', payload)
-}
+const BASE_URL = 'http://127.0.0.1:8000/api'
 
 export async function get_login_code(payload) {
-  return request.post(BASE_URL + '/api/get_login_code/', payload)
+  return request.get(BASE_URL + '/get_login_code/', payload)
 }
-
-export async function get_info(payload) {
-  return request.get(BASE_URL + '/api/get_info/', payload)
+export async function get_background_mage(payload) {
+  return request.get(BASE_URL + '/get_background_mage/', payload)
 }
-
-export async function insert_source(payload) {
-  return request.post(BASE_URL + '/api/insert_source/', payload)
-}
-
-export async function get_source_list(payload) {
-  return request.get(BASE_URL + '/api/get_source_list/', payload)
-}
-export async function update_info(payload) {
-  return request.get(BASE_URL + '/api/update_info/', payload)
-}
-
-export async function get_single_source(payload) {
-  return request.get(BASE_URL + '/api/get_single_source/', payload)
-}
-export async function get_collection(payload) {
-  return request.post(BASE_URL + '/api/get_collection/', payload)
-}
-export async function get_is_published(payload) {
-  return request.post(BASE_URL + '/api/get_is_published/', payload)
-}
-export async function update_published(payload) {
-  return request.post(BASE_URL + '/api/update_published/', payload)
-}
-export async function delete_published(payload) {
-  return request.post(BASE_URL + '/api/delete_published/', payload)
-}
-
-export async function search_source(payload) {
-  return request.get(BASE_URL + '/api/search_source/', payload)
-}
-export async function insert_comment(payload) {
-  return request.post(BASE_URL + '/api/insert_comment/', payload)
-}
-export async function get_comment_list(payload) {
-  return request.get(BASE_URL + '/api/get_comment_list/', payload)
-}
-
