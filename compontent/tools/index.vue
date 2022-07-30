f735800823d01760c8b84d31e3479cbadaa98fd2
yuanli <2865437316@qq.com>
2022-07-30 14:32:02 +0800

    工具

---
+++ compontent/tools/index.vue
@@ -0,0 +1,316 @@
+<template>
+  <view class="toolBox">
+    <view :class="[toolsArr.length ==0?'carouselBox':'carouselBox noCenter']">
+      <view v-for="i in toolsArr" class="item">
+        <view class="itemBox">
+          {{ i.icon }}
+        </view>
+        <span>{{ i.name }}</span>
+      </view>
+      <view v-for="i in toolsArr" class="item">
+        <view class="itemBox">
+          {{ i.icon }}
+        </view>
+        <span>{{ i.name }}</span>
+      </view>
+      <view v-for="i in toolsArr" class="item">
+        <view class="itemBox">
+          {{ i.icon }}
+        </view>
+        <span>{{ i.name }}</span>
+      </view>
+      <view v-for="i in toolsArr" class="item">
+        <view class="itemBox">
+          {{ i.icon }}
+        </view>
+        <span>{{ i.name }}</span>
+      </view>
+      <view v-for="i in toolsArr" class="item">
+        <view class="itemBox">
+          {{ i.icon }}
+        </view>
+        <span>{{ i.name }}</span>
+      </view>
+      <view v-for="i in toolsArr" class="item">
+        <view class="itemBox">
+          {{ i.icon }}
+        </view>
+        <span>{{ i.name }}</span>
+      </view>
+      <view v-for="i in toolsArr" class="item">
+        <view class="itemBox">
+          {{ i.icon }}
+        </view>
+        <span>{{ i.name }}</span>
+      </view>
+      <view v-for="i in toolsArr" class="item">
+        <view class="itemBox">
+          {{ i.icon }}
+        </view>
+        <span>{{ i.name }}</span>
+      </view>
+      <view v-for="i in toolsArr" class="item">
+        <view class="itemBox">
+          {{ i.icon }}
+        </view>
+        <span>{{ i.name }}</span>
+      </view>
+      <view v-for="i in toolsArr" class="item">
+        <view class="itemBox">
+          {{ i.icon }}
+        </view>
+        <span>{{ i.name }}</span>
+      </view>
+      <view v-for="i in toolsArr" class="item">
+        <view class="itemBox">
+          {{ i.icon }}
+        </view>
+        <span>{{ i.name }}</span>
+      </view>
+      <view v-for="i in toolsArr" class="item">
+        <view class="itemBox">
+          {{ i.icon }}
+        </view>
+        <span>{{ i.name }}</span>
+      </view>
+      <view v-for="i in toolsArr" class="item">
+        <view class="itemBox">
+          {{ i.icon }}
+        </view>
+        <span>{{ i.name }}</span>
+      </view>
+      <view v-for="i in toolsArr" class="item">
+        <view class="itemBox">
+          {{ i.icon }}
+        </view>
+        <span>{{ i.name }}</span>
+      </view>
+      <view v-for="i in toolsArr" class="item">
+        <view class="itemBox">
+          {{ i.icon }}
+        </view>
+        <span>{{ i.name }}</span>
+      </view>
+      <view v-for="i in toolsArr" class="item">
+        <view class="itemBox">
+          {{ i.icon }}
+        </view>
+        <span>{{ i.name }}</span>
+      </view>
+      <view v-for="i in toolsArr" class="item">
+        <view class="itemBox">
+          {{ i.icon }}
+        </view>
+        <span>{{ i.name }}</span>
+      </view>
+      <view v-for="i in toolsArr" class="item">
+        <view class="itemBox">
+          {{ i.icon }}
+        </view>
+        <span>{{ i.name }}</span>
+      </view>
+      <view v-for="i in toolsArr" class="item">
+        <view class="itemBox">
+          {{ i.icon }}
+        </view>
+        <span>{{ i.name }}</span>
+      </view>
+      <view v-for="i in toolsArr" class="item">
+        <view class="itemBox">
+          {{ i.icon }}
+        </view>
+        <span>{{ i.name }}</span>
+      </view>
+      <view v-for="i in toolsArr" class="item">
+        <view class="itemBox">
+          {{ i.icon }}
+        </view>
+        <span>{{ i.name }}</span>
+      </view>
+      <view v-for="i in toolsArr" class="item">
+        <view class="itemBox">
+          {{ i.icon }}
+        </view>
+        <span>{{ i.name }}</span>
+      </view>
+      <view v-for="i in toolsArr" class="item">
+        <view class="itemBox">
+          {{ i.icon }}
+        </view>
+        <span>{{ i.name }}</span>
+      </view>
+      <view v-for="i in toolsArr" class="item">
+        <view class="itemBox">
+          {{ i.icon }}
+        </view>
+        <span>{{ i.name }}</span>
+      </view>
+      <view v-for="i in toolsArr" class="item">
+        <view class="itemBox">
+          {{ i.icon }}
+        </view>
+        <span>{{ i.name }}</span>
+      </view>
+      <view v-for="i in toolsArr" class="item">
+        <view class="itemBox">
+          {{ i.icon }}
+        </view>
+        <span>{{ i.name }}</span>
+      </view>
+      <view v-for="i in toolsArr" class="item">
+        <view class="itemBox">
+          {{ i.icon }}
+        </view>
+        <span>{{ i.name }}</span>
+      </view>
+      <view v-for="i in toolsArr" class="item">
+        <view class="itemBox">
+          {{ i.icon }}
+        </view>
+        <span>{{ i.name }}</span>
+      </view>
+      <view class="item">
+        <view class="itemBox">
+          <svg t="1659162445218" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg"
+               p-id="21360" width="48" height="48">
+            <path
+                d="M863.328262 481.340895l-317.344013 0.099772L545.984249 162.816826c0-17.664722-14.336138-32.00086-32.00086-32.00086s-31.99914 14.336138-31.99914 32.00086l0 318.400215-322.368714-0.17718c-0.032684 0-0.063647 0-0.096331 0-17.632039 0-31.935493 14.239806-32.00086 31.904529-0.096331 17.664722 14.208843 32.031824 31.871845 32.095471l322.59234 0.17718 0 319.167424c0 17.695686 14.336138 32.00086 31.99914 32.00086s32.00086-14.303454 32.00086-32.00086L545.982529 545.440667l317.087703-0.099772c0.063647 0 0.096331 0 0.127295 0 17.632039 0 31.935493-14.239806 32.00086-31.904529S880.960301 481.404542 863.328262 481.340895z"
+                p-id="21361" fill="#f7f6f6"></path>
+          </svg>
+        </view>
+        添加快捷访问
+      </view>
+    </view>
+  </view>
+</template>
+
+<script>
+export default {
+  name: "Tools",
+  data() {
+    return {
+      toolsArr: [
+        {
+          name: '翻译',
+          icon: 'icon',
+          url: 'https://fanyi.baidu.com/'
+        }
+      ]
+    }
+  },
+
+}
+</script>
+
+<style scoped>
+.toolBox {
+  position: absolute;
+  margin-top: 230px;
+  left: 50%;
+  transform: translateX(-50%);
+  max-width: 800px;
+  width: 100%;
+  height: calc(100vh - 330px);
+  overflow: hidden;
+  transition: .25s;
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+}
+
+.carouselBox {
+  max-height: 100%;
+  max-width: 100%;
+  color: #fff;
+  overflow-y: auto;
+  transition: .25s;
+  margin: 0 auto;
+  padding: 2px;
+  width: 100%;
+  display: grid;
+  justify-content: center;
+  height: 100%;
+}
+
+.noCenter {
+  grid-template-columns: repeat(auto-fill, 120px);
+  height: unset;
+}
+
+.carouselBox::-webkit-scrollbar {
+  display: none
+}
+
+.itemBox {
+  position: relative;
+  /*display: inline-block;*/
+  width: 80px;
+  height: 80px;
+  margin: 20px 26px 10px;
+  border-radius: 15px;
+  text-align: center;
+  cursor: pointer;
+  transition: background-color .35s;
+  background-color: rgba(225, 225, 225, .5);
+  backdrop-filter: blur(30px);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.itemBox {
+  -webkit-backdrop-filter: blur(30px) !important;
+  backdrop-filter: blur(30px) !important;
+}
+
+.itemBox:hover {
+  background-color: rgba(225, 225, 225, .7);
+}
+
+@media screen and (max-width: 350px) {
+  .itemBox {
+    margin-bottom: 20px;
+  }
+}
+
+@media screen and (max-width: 500px) {
+  .itemBox {
+    height: 50px;
+    width: 50px;
+  }
+
+  .carouselBox {
+    grid-template-columns: repeat(auto-fill, 90px);
+  }
+}
+
+@media screen and (max-width: 350px) {
+  .itemBox {
+    margin-bottom: 20px;
+  }
+}
+
+@media screen and (max-width: 600px) {
+  .itemBox {
+    width: 60px;
+    height: 60px;
+    margin: 15px 15px 10px 15px;
+  }
+}
+
+.item {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  color: #fff;
+  font-size: 12px;
+  overflow: hidden;
+  text-align: center;
+  text-overflow: ellipsis;
+  transition: .25s;
+  white-space: nowrap;
+  width: 90%;
+  margin: 5px;
+}
+</style>
