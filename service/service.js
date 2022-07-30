4a35d72aba17f69f7a6260175f6d101cf7466281
yuanli <2865437316@qq.com>
2022-07-30 17:11:04 +0800

    验证

--- service/service.js
+++ service/service.js
@@ -1,5 +1,7 @@
 import request from '../service/request'
-const BASE_URL = 'http://127.0.0.1:8000/api'
+import getApiBaseUrl from '../utils/base_api_url'
+// const BASE_URL = 'http://127.0.0.1:8000/api'
+const BASE_URL = 'http://8.136.80.201/api'
 
 export async function get_login_code(payload) {
   return request.get(BASE_URL + '/get_login_code/', payload)
--- utils/base_api_url.js
+++ utils/base_api_url.js
@@ -1,16 +1,14 @@
+
 const getApiBaseUrl = () => {
   let BASE_URL;
-  // if (process.env.NODE_ENV === 'development') {
-  //   BASE_URL='http://82.157.175.26:8080'
-  // }
-  // else {
-  //   BASE_URL='http://82.157.175.26:8080'
-  //
-  //   if(process.env.TEST=== 'test'){
-  //
-  //   }
-  // }
-
+  if (process.env.NODE_ENV === 'development') {
+    BASE_URL='http://127.0.0.1:8000/api'
+  }
+  else {
+    BASE_URL='http://8.136.80.201/api'
+  }
   return BASE_URL
 }
+
+
 export default getApiBaseUrl;
